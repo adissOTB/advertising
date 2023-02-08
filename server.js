c3e25ba646fd81582d9868d2a2bd128685749c88
adissOTB <sandras24354910@gmail.com>
2023-02-08 21:52:22 +0200

    Recent updates

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const path = require('path');
 
-app.use(express);
+app.use(express.static(__dirname));
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname + 'index.js'));
+})
 
 app.listen(port, function(req, res) {
     console.log('Listening on port ' + port);
