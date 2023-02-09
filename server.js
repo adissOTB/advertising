a540474964fdcbb1e45171d4195ccd4e4dfdeece
adissOTB <sandras24354910@gmail.com>
2023-02-09 22:51:09 +0200

    Database connected and tested

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
+var mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
 
 app.use(express.static(__dirname));
+
+mongoose.connect('mongodb://localhost:27017/Banners', function(req, res) {
+    if(mongoose){
+        console.log('Database successfully connected!!');
+    } else{
+        console.log('Unable to connect to the database');
+    }
+})
 
 app.get('*', function(req, res){
     res.sendFile(path.join(__dirname + 'index.js'));
